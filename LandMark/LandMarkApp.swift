6efd3b1ddc5dcb5b16c4d6ec6fc7e4732e49ecd7
PrakharSaki <prakhar@nunify>
2024-02-09 15:51:10 +0530

    Initial Commit

---
+++ LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by Prakhar Saki on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
